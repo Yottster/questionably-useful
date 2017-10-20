47c5876467c6a4487965665f8fed626c4d6a573b
Viktor Stenqvist <viktor.stenqvist@mathem.se>
2017-10-20 16:46:29 +0200

    Initial commit

---
+++ src/utils.ps1
@@ -0,0 +1,7 @@
+﻿function Search-Files($term) {
+    return Get-ChildItem '.' -Recurse -ErrorAction SilentlyContinue | Select-String -pattern $term
+}
+
+function Open-SearchResult($term) {
+    return Search-Files($term) | %{ code --goto "$($_.path):$($_.linenumber)" }
+}
